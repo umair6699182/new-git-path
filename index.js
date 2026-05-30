cda1ccdb1bcebff2dd90a02970e659ede224ce31
umair6699182 <um83414@gmail.com>
2026-05-30 23:48:52 +0500

    New commit add

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
